429d4d08cd76d8807191acb4934afe12c4ffe19f
Santiago Iannelli <santiagoiannelli2@gmail.com>
2022-11-08 16:20:02 -0300

    primer commit

--- src/main/java/com/santander/homebanking/HomebankingApplication.java
+++ src/main/java/com/santander/homebanking/HomebankingApplication.java
@@ -74,6 +74,7 @@ public class HomebankingApplication {
 			//Loan loan1 = new Loan("Hipotecario",500000,new ArrayList<>(Arrays.asList(6,12,24,48)));
 
 			//ClientLoan clientLoan1 = new ClientLoan(500000,new ArrayList<>(Arrays.asList(6,12,24,48)),cliente1,loan1);
+			// commit
 
 		};
 	}
