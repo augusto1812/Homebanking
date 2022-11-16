36945ad76b7245d264224da5d6905347c3d74a38
megafagol <fabriziorecchini9908@gmail.com>
2022-11-16 17:36:38 -0300

    arreglo de mensajes de error

--- src/main/java/com/santander/homebanking/services/TransactionService.java
+++ src/main/java/com/santander/homebanking/services/TransactionService.java
@@ -20,6 +20,9 @@ import java.util.Arrays;
 public class TransactionService {
 
     @Autowired
+    MessageService messageService;
+
+    @Autowired
     AccountRepository accountRepository;
 
     @Autowired
@@ -27,8 +30,6 @@ public class TransactionService {
 
     @Autowired
     TransactionRepository transactionRepository;
-    @Autowired
-    private MessageService messageService;
 
 
 
@@ -63,7 +64,7 @@ public class TransactionService {
 
         //Verificar que la cuenta de origen y de destino sen del mismo tipo (accountType) (CA y CC)
         if(!(accountRepository.findByNumber(accountFrom).orElse(null).getAccountType().equals(accountRepository.findByNumber(accountTo).orElse(null).getAccountType()))){
-            return new ResponseEntity<>(getMensaje("transaction.createTransaction.differentAccountType"), HttpStatus.FORBIDDEN);
+            return new ResponseEntity<>(messageService.getMessage("transaction.createTransaction.differentAccountType"), HttpStatus.FORBIDDEN);
         }
 
         //Verificar que la cuenta de origen tenga el monto disponible.
@@ -123,51 +124,51 @@ public class TransactionService {
 
         //Verificar que los parámetros no estén vacíos
         if(amountSale.isNaN() || amountBuy.isNaN() || accountFrom.isBlank() || accountTo.isBlank()){
-            return new ArrayList<>(Arrays.asList(1,getMensaje("transaction.createTransaction.emptyValues"),403));
+            return new ArrayList<>(Arrays.asList(1,messageService.getMessage("transaction.createTransaction.emptyValues"),403));
         }
 
         //Verificar que los números de cuenta no sean iguales
         if(accountFrom.equals(accountTo)){
-            return new ArrayList<>(Arrays.asList(1,getMensaje("transaction.createTransaction.equalAccounts"),403));
+            return new ArrayList<>(Arrays.asList(1,messageService.getMessage("transaction.createTransaction.equalAccounts"),403));
         }
 
         //Verificar que exista la cuenta de origen
         Account accFrom = accountRepository.findByNumber(accountFrom).orElse(null);
         if (accFrom == null){
-            return new ArrayList<>(Arrays.asList(1,getMensaje("transaction.createTransaction.missingAccountFrom"),403));
+            return new ArrayList<>(Arrays.asList(1,messageService.getMessage("transaction.createTransaction.missingAccountFrom"),403));
         }
 
         //Verificar que exista la cuenta de destino
         Account accTo = accountRepository.findByNumber(accountTo).orElse(null);
         if (accTo == null){
-            return new ArrayList<>(Arrays.asList(1,getMensaje("transaction.createTransaction.missingAccountTo"),403));
+            return new ArrayList<>(Arrays.asList(1,messageService.getMessage("transaction.createTransaction.missingAccountTo"),403));
         }
 
         //Verificar que la cuenta de origen y destino pertenezca al cliente autenticado
         Client client = clientRepository.findByEmail(authentication.getName()).orElse(null);
         if(accountRepository.findByClientAndNumber(client, accountFrom).orElse(null) ==  null || accountRepository.findByClientAndNumber(client, accountTo).orElse(null) ==  null){
-            return new ArrayList<>(Arrays.asList(1,getMensaje("transaction.createTransactionBuySaleCurr.missingAccountFromOrAccountTo"),403));
+            return new ArrayList<>(Arrays.asList(1,messageService.getMessage("transaction.createTransactionBuySaleCurr.missingAccountFromOrAccountTo"),403));
         }
 
         //Verificar que la cuenta de origen y de destino sen del mismo tipo (accountType) (CA y CC)
         if(!(accFrom.getAccountType().equals(accTo.getAccountType()))){
-            return new ArrayList<>(Arrays.asList(1,getMensaje("transaction.createTransaction.differentAccountType"),403));
+            return new ArrayList<>(Arrays.asList(1,messageService.getMessage("transaction.createTransaction.differentAccountType"),403));
         }
 
         //Verificar si la accFrom es del tipo currSale
         if(!(accFrom.getCurrencyType().equals(CurrencyType.valueOf(currSale)))){
-            return new ArrayList<>(Arrays.asList(1,getMensaje("transaction.createTransactionBuySaleCurr.accFromTypeWrong"),403));
+            return new ArrayList<>(Arrays.asList(1,messageService.getMessage("transaction.createTransactionBuySaleCurr.accFromTypeWrong"),403));
         }
 
         //Verificar si la accTo es del tipo currBuy
         if(!(accTo.getCurrencyType().equals(CurrencyType.valueOf(currBuy)))){
-            return new ArrayList<>(Arrays.asList(1,getMensaje("transaction.createTransactionBuySaleCurr.accToTypeWrong"),403));
+            return new ArrayList<>(Arrays.asList(1,messageService.getMessage("transaction.createTransactionBuySaleCurr.accToTypeWrong"),403));
         }
 
         //Verificar que la cuenta de origen tenga el monto disponible.
         Double availableAmountFrom = accountRepository.findByNumber(accountFrom).orElse(null).getBalance();
         if(availableAmountFrom < amountSale){
-            return new ArrayList<>(Arrays.asList(1,getMensaje("transaction.createTransaction.poorMoney"),403));
+            return new ArrayList<>(Arrays.asList(1,messageService.getMessage("transaction.createTransaction.poorMoney"),403));
         }
 
         //Crear transacción “DEBIT” asociada a la cuenta de origen
@@ -189,7 +190,7 @@ public class TransactionService {
         accountRepository.save(accTo);
         transactionRepository.save(debit);
         transactionRepository.save(credit);
-        return new ArrayList<>(Arrays.asList(0,getMensaje("transaction.createTransaction.created"),201));
+        return new ArrayList<>(Arrays.asList(0,messageService.getMessage("transaction.createTransaction.created"),201));
     }
 
 }
