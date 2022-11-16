846ef79b3e802967f4687011ae47a9d91551e6d1
megafagol <fabriziorecchini9908@gmail.com>
2022-11-16 10:54:54 -0300

    front de cotizaciones listo

--- src/main/resources/static/web/js/currency-price.js
+++ src/main/resources/static/web/js/currency-price.js
@@ -1,80 +1,18 @@
 var app = new Vue({
     el:"#app",
     data:{
-        clientAccounts: [],
-        clientAccountsTo: [],
-        debitCards: [],
         errorToats: null,
         errorMsg: null,
-        accountFromNumber: "VIN",
-        accountToNumber: "VIN",
-        trasnferType: "own",
-        amount: 0,
-        description: "",
         typesCurrencies:[],
         firstCurrency:"",
         secondCurrency:"",
-        priceData:{}
+        priceData:{},
+        firstCurrencyPrice:"",
+        secondCurrencyPrice:""
     },
     methods:{
-        getData: function(){
-            axios.get("/api/clients/current/accounts")
-            .then((response) => {
-                //get client ifo
-                this.clientAccounts = response.data;
-            })
-            .catch((error) => {
-                this.errorMsg = "Error getting data";
-                this.errorToats.show();
-            })
-        },
         formatDate: function(date){
             return new Date(date).toLocaleDateString('en-gb');
-        },
-        checkTransfer: function(){
-            if(this.accountFromNumber == "VIN"){
-                this.errorMsg = "You must select an origin account";
-                this.errorToats.show();
-            }
-            else if(this.accountToNumber == "VIN"){
-                this.errorMsg = "You must select a destination account";
-                this.errorToats.show();
-            }else if(this.amount == 0){
-                this.errorMsg = "You must indicate an amount";
-                this.errorToats.show();
-            }
-            else if(this.description.length <= 0){
-                this.errorMsg = "You must indicate a description";
-                this.errorToats.show();
-            }else{
-                this.modal.show();
-            }
-        },
-        transfer: function(){
-            let config = {
-                /*headers: {
-                    'content-type': 'application/x-www-form-urlencoded'
-                }*/
-            }
-            axios.post(`/api/transactions?fromAccountNumber=${this.accountFromNumber}&toAccountNumber=${this.accountToNumber}&amount=${this.amount}&description=${this.description}`,config)
-            .then(response => {
-                this.modal.hide();
-                this.okmodal.show();
-            })
-            .catch((error) =>{
-                this.errorMsg = error.response.data;
-                this.errorToats.show();
-            })
-        },
-        changedType: function(){
-            this.accountFromNumber = "VIN";
-            this.accountToNumber = "VIN";
-        },
-        changedFrom: function(){
-            if(this.trasnferType == "own"){
-                this.clientAccountsTo = this.clientAccounts.filter(account => account.number != this.accountFromNumber);
-                this.accountToNumber = "VIN";
-            }
         },
         finish: function(){
             window.location.reload();
@@ -98,44 +36,22 @@ var app = new Vue({
                 this.errorToats.show();
             })
         },
-        getPrice: function(){
-            const urlParams = new URLSearchParams(window.location.search);
-            const currBuy = urlParams.get('currBuy');
-            const currSale = urlParams.get('currSale');
-            axios.get("/api/currency/getPrice/"+currBuy+"/"+currSale)
-            .then((response) => {
-                //get client ifo
-                this.priceData = response.data;
-            })
-            .catch((error) => {
-                this.errorMsg = "Error getting data";
-                this.errorToats.show();
-            })
-        },
         getPricePage: function(){
-//            url = "/currency-prices.html?currBuy="+this.firstCurrency+"&currSale="+this.secondCurrency;
-//            window.location.href="http://localhost:8080/web/currency-prices.html?currBuy=usd&currSale=ars";
-//            url2=`currency-prices.html?currBuy=${this.firstCurrency}&currSale=${this.secondCurrency}`;
-//            url3='currency-prices.html?currBuy=usd&currSale=ars';
-//            window.location.replace('currency-prices.html?currBuy=usd&currSale=ars');
-               const currBuyAux = this.firstCurrency;
-               const currSaleAux= 'ars';
-
-
-
-               //window.location.replace('currency-prices.html?currBuy='+this.firstCurrency+'&currSale=ars');
-
-
-                //funka
-               window.location.replace('currency-prices.html?currBuy=usd&currSale=ars');
+               axios.get("/api/currency/getPrice/"+this.firstCurrency+"/"+this.secondCurrency)
+               .then((response) => {
+                   this.firstCurrencyPrice = response.data.compra;
+                   this.secondCurrencyPrice = response.data.venta;
+               })
+               .catch((error) => {
+                   this.errorMsg = error.response.data;
+                   this.errorToats.show();
+               })
         },
     },
     mounted: function(){
         this.errorToats = new bootstrap.Toast(document.getElementById('danger-toast'));
         this.modal = new bootstrap.Modal(document.getElementById('confirModal'));
         this.okmodal = new bootstrap.Modal(document.getElementById('okModal'));
-        this.getData();
         this.getTypesCurrencies();
-        this.getPrice();
     }
 })
