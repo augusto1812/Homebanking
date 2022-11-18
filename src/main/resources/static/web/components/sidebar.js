b4b89eb13258c0f26f8e7dcb931cf1de7d96cdf3
MariaEmiliaWalter <79611670+MariaEmiliaWalter@users.noreply.github.com>
2022-11-18 11:28:10 -0300

    correccion sidebar

--- src/main/resources/static/web/components/sidebar.js
+++ src/main/resources/static/web/components/sidebar.js
@@ -1,4 +1,3 @@
-//import * from '../js/investments.js';
 Vue.component('sidebar',{
     props: {
         name: String
@@ -71,12 +70,29 @@ Vue.component('sidebar',{
                                         </li>
                                     </ul>
                                     <hr>
-                                    <ul class="nav nav-pills flex-column mb-auto">
-                                        <button class="btn btn-danger img-fluid" v-on:click="signOut">Sign out</button>
+
+                                <div class="dropdown">
+                                    <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle"
+                                       id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
+                                        <strong> nombre</strong>
+                                    </a>
+                                    <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
+
+                                        <li><a class="dropdown-item" href="#">Profile</a></li>
+                                        <li>
+                                            <hr class="dropdown-divider">
+                                        </li>
+                                        <li><a class="dropdown-item btn btn-danger img-fluid" v-on:click="signOut" >Sign out</a></li>
                                     </ul>
+                                </div>
+
                                 </div>
                             </div> `,
 
   });
 
 
+//<ul class="nav nav-pills flex-column mb-auto">
+//    <button class="btn btn-danger img-fluid" v-on:click="signOut">Sign out</button>
+//</ul>
